24220907c677ec17935440ea5201f3e9fb5267e7
kranthikumar9625 <kranthikumar9625@gmail.com>
2020-07-03 16:58:56 +0530

    developed logic building query

--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import pandas as pd
 from config import DB_DETAILS
 import sqlalchemy
 import pymysql
+import psycopg2
 
 
 def get_tables(path):
@@ -15,13 +16,27 @@ def get_mysql_connection(db_host,db_port,db_name,db_user,db_pass):
     try:
         connection = sqlalchemy.create_engine(f"mysql+pymysql://%s:%s@%s:%s/%s" \
                                               % (db_user, db_pass, db_host, db_port, db_name))
-        print("connection to database is succesfull")
+        print("connection to mysql database is succesfull")
         return connection
     except sqlalchemy.exc.SQLAlchemyError as e:
         print(type(e))
+
+def get_postgresql_coinnection(db_host,db_port,db_name,db_user,db_pass):
+    try:
+        connection = sqlalchemy.create_engine(f"postgresql+psycopg2://%s:%s@%s:%s/%s" \
+                                              % (db_user, db_pass, db_host, db_port, db_name))
+        print("connection to postgresql database is succesfull")
+        return connection
+    except sqlalchemy.exc.SQLAlchemyError as e:
+        print(type(e))
+
+
 def get_connection(db_type,db_host,db_port,db_name,db_user,db_pass):
     connection = None
     if db_type == 'mysql':
         connection = get_mysql_connection(db_host,db_port,db_name,db_user,db_pass)
+        return connection
+    if db_type == 'postgresql':
+        connection = get_mysql_connection(db_host, db_port, db_name, db_user, db_pass)
         return connection
 
--- write.py
+++ write.py
@@ -1,1 +1,8 @@
 #writing the data to db
+from util import get_connection
+import sqlalchemy
+
+def build_query(table_name,column_names):
+    column_values = tuple(map(lambda column:column.replace(column,'%s')))
+    query = sqlalchemy.text(f'Insert INTO {table_name} {column_names} VALUES {column_values}')
+    return query
